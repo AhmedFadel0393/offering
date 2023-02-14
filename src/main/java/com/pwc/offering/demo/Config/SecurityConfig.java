4aabe6319535fc1be2248d9194ed35dfdcf38f6a
AhmedFadel0393 <ahmed.fadel0393@gmail.com>
2023-02-14 14:07:40 +0200

    22

--- src/main/java/com/pwc/offering/demo/Config/SecurityConfig.java
+++ src/main/java/com/pwc/offering/demo/Config/SecurityConfig.java
@@ -1,21 +1,23 @@
 package com.pwc.offering.demo.Config;
 
-//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
-//import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 //        http.authorizeRequests().antMatchers("/**").permitAll();
-////        http.antMatcher("/**").authorizeRequests().anyRequest().authenticated().and().oauth2Login();
-//        return http.build();
-//    }
+        http.antMatcher("/**").authorizeRequests().antMatchers("/swagger-ui/index.html").permitAll();
+
+//        http.antMatcher("/**").authorizeRequests().anyRequest().authenticated().and().oauth2Login();
+        return http.build();
+    }
 
 }
--- src/main/java/com/pwc/offering/demo/controller/AuthController.java
+++ src/main/java/com/pwc/offering/demo/controller/AuthController.java
@@ -1,8 +1,8 @@
 package com.pwc.offering.demo.controller;
 
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.oauth2.core.user.OAuth2User;
-//import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
@@ -11,9 +11,9 @@ import java.util.Map;
 @RestController
 public class AuthController {
 
-//    @GetMapping("/CurrentUser")
-//    public Map<String, Object> user() {
-//        OAuth2User principal = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        return Collections.singletonMap("name", principal.getAttribute("name")+" - "+principal.getAttribute("email"));
-//    }
+    @GetMapping("/CurrentUser")
+    public Map<String, Object> user() {
+        OAuth2User principal = (OAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return Collections.singletonMap("name", principal.getAttribute("name")+" - "+principal.getAttribute("email"));
+    }
 }
