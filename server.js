815977d88432415218d130cad0865d742a4ef7d0
AhmedFadel0393 <ahmed.fadel0393@gmail.com>
2023-02-21 16:19:23 +0200

    55555

--- server.js
+++ server.js
@@ -3,16 +3,7 @@ const { exec } = require('child_process');
 const app = express();
 
 app.get('/', (req, res) => {
-  exec('gcloud app logs tail --project fadel-test-reactongcp -s java-be', (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      res.send(`Error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-    res.send(`<pre>${stdout}</pre>`);
-  });
+  exec('gcloud app logs tail --project fadel-test-reactongcp -s java-be');
 });
 
 const PORT = process.env.PORT || 8080;
